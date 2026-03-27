e075c64da4b0b844c1e2ec93063d9dfc48dae012
Jyothi Sai Swaroop <jyothisaiswaroopakki@gmail.com>
2026-03-27 15:47:42 +0000

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,10 +5,7 @@ BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 
 def get_weather(city):
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric"}
+    params = {"q": city,"appid": API_KEY,"units": "metric"}
     response = requests.get(BASE_URL, params=params)
 
     if response.status_code == 200:
